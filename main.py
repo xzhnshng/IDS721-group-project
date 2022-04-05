04f439a9940f1019d423efb06005a73e2e9d81ae
xzhnshng <zhnshngxie@gmail.com>
2022-04-05 01:54:18 -0400

    complete containizatio

--- main.py
+++ main.py
@@ -25,5 +25,5 @@ def predict():
     return render_template('index.html', prediction_text='Gold Medal Number is {}'.format(output))
 
 if __name__ == "__main__":
-    #app.run(host='127.0.0.1', port=8080, debug=True)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    # app.run(debug=True)
