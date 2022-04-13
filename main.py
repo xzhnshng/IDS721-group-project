b53a5a6ec4fb9312f6357a46fac71e2500c61edf
Qishen <qw98@duke.edu>
2022-04-12 22:09:43 -0400

    comment

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    print("hello")
+    print("Hello Duke")
     return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
